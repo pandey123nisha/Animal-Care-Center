0aea1bdf9431e889364d85ef4a0f45b5a533ae0a
Sajal Shrestha <codeitsajal@gmail.com>
2022-12-07 12:30:33 +0545

    minor changes

--- src/components/LearnMore.vue
+++ src/components/LearnMore.vue
@@ -1,7 +1,28 @@
 <template>
-    <div>
-       
-    </div>
+    <vs-card>
+    <template #title>
+      <h3>Dog Treatment</h3>
+    </template>
+    <template #img>
+      <img src="../assets/img2.jpg" alt="" class="img-fluid">
+    </template>
+    <template #text>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      </p>
+    </template>
+    <template #interactions>
+      <vs-button danger icon>
+        <i class='bx bx-heart'></i>
+      </vs-button>mino
+      <vs-button class="btn-chat" shadow primary>
+        <i class='bx bx-chat' ></i>
+        <span class="span">
+          54
+        </span>
+      </vs-button>
+    </template>
+  </vs-card>
 </template>
 
 <script>
