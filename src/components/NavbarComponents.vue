4be3a28dfceca603873a0ce223a708652a8cd871
Sajal Shrestha <codeitsajal@gmail.com>
2022-12-08 10:07:44 +0545

    minor change

--- src/components/NavbarComponents.vue
+++ src/components/NavbarComponents.vue
@@ -1,68 +1,118 @@
 <template>
-    <div>
-      <!-- head header -->
-        <div class="topNavBar py-2">
-          <div class="container d-flex justify-content-center align-items-center">
-              <div class="text-white">Animal Care Center (+977-9862130505)</div>
-              <div class="mx-md-5">
-                  <a href="" class="btn appointment">Book an appointment</a>
-              </div>
-          </div>
+  <div>
+    <!-- head header -->
+    <div class="topNavBar py-2">
+      <div class="container d-flex justify-content-center align-items-center">
+        <div class="text-white">Animal Care Center (+977-9862130505)</div>
+        <div class="mx-md-5">
+          <vs-button
+        warn
+        gradient
+        :active="active == 3"
+        @click="active = 3"
+      >
+        <i class="bx bxs-bell-ring"></i> Book an Appointment
+      </vs-button>
         </div>
+      </div>
+    </div>
 
-        <nav class="navbar navbar-expand-lg bg-light">
-  <div class="container">
-    <a class="navbar-brand" href="#"><img src="../assets/logo (1).png" alt="Max-width 100%" style="height: 45px; "></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#"   @click.prevent="$router.push({ name: 'home' })">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#"   @click.prevent="$router.push({ name: 'about' })">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#"   @click.prevent="$router.push({ name: 'contact' })">Contact</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#"   @click.prevent="$router.push({ name: 'service' })">Service</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active " aria-current="page" href="#"  @click.prevent="$router.push({ name: 'doctors' })">Doctors</a>
-        </li>
-        <div class="input-group rounded">
-          <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-          <span class="input-group-text border-0" id="search-addon">
-            <i class="fas fa-search"></i>
-          </span>
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#"
+          ><img
+            src="../assets/logo (1).png"
+            alt="Max-width 100%"
+            style="height: 45px"
+        /></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a
+                class="nav-link active"
+                aria-current="page"
+                href="#"
+                @click.prevent="$router.push({ name: 'home' })"
+                >Home</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link active"
+                aria-current="page"
+                href="#"
+                @click.prevent="$router.push({ name: 'about' })"
+                >About</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link active"
+                aria-current="page"
+                href="#"
+                @click.prevent="$router.push({ name: 'contact' })"
+                >Contact</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link active"
+                aria-current="page"
+                href="#"
+                @click.prevent="$router.push({ name: 'service' })"
+                >Service</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link active"
+                aria-current="page"
+                href="#"
+                @click.prevent="$router.push({ name: 'doctors' })"
+                >Doctors</a
+              >
+            </li>
+            <div class="input-group rounded">
+              <input
+                type="search"
+                class="form-control rounded"
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="search-addon"
+              />
+              <span class="input-group-text border-0" id="search-addon">
+                <i class="fas fa-search"></i>
+              </span>
+            </div>
+          </ul>
         </div>
-      </ul>
-    </div>
+      </div>
+    </nav>
   </div>
-</nav>
-    </div>
 </template>
 
 <script>
 export default {
-    name: "NavbarComponent",
-    
-}
+  name: "NavbarComponent",
+};
 </script>
 
 <style scoped>
-
-
-.appointment{
-    color: #7F0000;
-    background-color: white;
-    font-weight: bold;
-    padding: 0px 10px 0px 10px
+.appointment {
+  color: #7f0000;
+  background-color: white;
+  font-weight: bold;
+  padding: 0px 10px 0px 10px;
 }
-
-
-
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,144 +1,162 @@
 <template>
   <template-view>
-   <!-- Section 1 -->
-   <div>
-    <div class="row">
-      <div class="col-md-12">
-        <carousel-component/>
-      </div>
-    </div>
-   </div>
-
-   <!-- Section 2 -->
-   <div class="py-5 bg-light">
-    <div class="container">
+    <!-- Section 1 -->
+    <div>
       <div class="row">
-        <div class="col-md-6">
-          <img src="../assets/Dental-Care-Section (1).jpg" class="img-fluid rounded-3">
-        </div>
-        <div class="col-md-6">
-          <div class="d-flex align-items-center h-100">
-            <div class="">
-            <h2  class="">Complete Wellness Care</h2>
-            <p>Routine wellness exams are a critical part of your pet’s healthcare routine to maintain 
-              their wellbeing and detect any medical issues before they progress. 
-              During your pet’s wellness exam, we will conduct an in-depth, nose to tail 
-              examination and advise you on all aspects of your pet's care.</p>
-              <h3 class="">Comprehensive Care</h3>
-              <div class="row">
-                <div class="col-md-4">
-                    <h6>+ Preventative Services</h6>
-                </div>
-                <div class="col-md-4">
-                    <h6>+ Surgical Services</h6>
-                </div>
-                <div class="col-md-4">
-                    <h6>+ Diagnostics</h6>
-                </div>
-              </div>
-              <div class="py-3">
-                <button type="button" class="btn ">View Our services</button>
-              </div>
-          </div>
-          </div>
-          
+        <div class="col-md-12">
+          <carousel-component />
         </div>
       </div>
     </div>
-   </div>
 
-   <!-- Section 3 -->
-   <div class="py-5 ">
-    <h2 style="text-align:center;">Pets Must Haves</h2>
-    <div class="container py-5">
-      <div class="row">
-        <div class="col-md-2">
-          <img src="../assets/hemp oil.jpg" alt="" class="pet-img">
-          <h6>Hemp Oil For Pets</h6>
-        </div>
-        <div class="col-md-2">
-          <img src="../assets/BrushySticks.jpg" alt="" class="pet-img">
-          <h6>Pet Hip & Joint Supplements</h6>
-        </div>
-        <div class="col-md-2">
-        <img src="../assets/allergyitch-pouch-1.jpg" alt="" class="pet-img">
-        <h6>Pets Allergy Supplements</h6>
-        </div>
-        <div class="col-md-2">
-          <img src="../assets/SeniorDog.jpg" alt="" class="pet-img">
-          <h6>Pets Dental Care</h6>
-        </div>
-        <div class="col-md-2">
-          <img src="../assets/HipJoint-1.jpg" alt="" class="pet-img">
-          <h6>Senior Pet Care & Pain Relief</h6>
-        </div>
-        <div class="col-md-2">
-          <img src="../assets/FoodTopper.jpg" alt="" class="pet-img">
-          <h6>All Natural Pet Food Toppers</h6>
-        </div>
-      </div>
-    </div>
-   </div>
-
-   <div class="bgPrimary py-5">
+    <!-- Section 2 -->
+    <div class="py-5 bg-light">
       <div class="container">
         <div class="row">
           <div class="col-md-6">
-              <div class="d-flex align-items-center h-100 ">
-                  <div>
-                      <h1 class="text-white">Welcome New Clients</h1>
-            <p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad voluptas harum impedit neque. Aspernatur, vero iste sequi, dicta commodi dolore ad iure laudantium vel tenetur quidem ipsam placeat sapiente maiores! Eius aspernatur possimus harum molestias? Aliquid eveniet aspernatur recusandae consequatur.</p>
-                  </div>
-              </div>
+            <img
+              src="../assets/Dental-Care-Section (1).jpg"
+              class="img-fluid rounded-3"
+            />
           </div>
-
           <div class="col-md-6">
-            <img src="../assets/dog_meow-removebg-preview.png" alt="" class="img-fluid">
+            <div class="d-flex align-items-center h-100">
+              <div class="">
+                <h2 class="">Complete Wellness Care</h2>
+                <p>
+                  Routine wellness exams are a critical part of your pet’s
+                  healthcare routine to maintain their wellbeing and detect any
+                  medical issues before they progress. During your pet’s
+                  wellness exam, we will conduct an in-depth, nose to tail
+                  examination and advise you on all aspects of your pet's care.
+                </p>
+                <h3 class="">Comprehensive Care</h3>
+                <div class="row">
+                  <div class="col-md-4">
+                    <h6>+ Preventative Services</h6>
+                  </div>
+                  <div class="col-md-4">
+                    <h6>+ Surgical Services</h6>
+                  </div>
+                  <div class="col-md-4">
+                    <h6>+ Diagnostics</h6>
+                  </div>
+                </div>
+                <div class="py-3">
+                  <vs-button
+                    circle
+                    color="#7F0000"
+                    
+                    :active="active == 2"
+                    @click="active = 2"
+                  >
+                    <i class="bx bxs-heart"></i> View Our services
+                  </vs-button>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-   </div>
-
-
-   <!-- Section 5 -->
-   <div class="py-5">
-    <h2 style="text-align:center">Learn more about Animal Care Center</h2>
-    <div class="container py-5">
-      <div class="row">
-        <div class="col-md-3">
-            <learn-more/>
-        </div>
-        <div class="col-md-3">
-            <learn-more/>
-
-        </div>
-        <div class="col-md-3">
-            <learn-more/>
-         
-        </div>
-        <div class="col-md-3">
-            <learn-more/>
-          
         </div>
       </div>
     </div>
-   </div>
-   
+
+    <!-- Section 3 -->
+    <div class="py-5">
+      <h2 style="text-align: center">Pets Must Haves</h2>
+      <div class="container py-5">
+        <div class="row">
+          <div class="col-md-2">
+            <img src="../assets/hemp oil.jpg" alt="" class="pet-img" />
+            <h6>Hemp Oil For Pets</h6>
+          </div>
+          <div class="col-md-2">
+            <img src="../assets/BrushySticks.jpg" alt="" class="pet-img" />
+            <h6>Pet Hip & Joint Supplements</h6>
+          </div>
+          <div class="col-md-2">
+            <img
+              src="../assets/allergyitch-pouch-1.jpg"
+              alt=""
+              class="pet-img"
+            />
+            <h6>Pets Allergy Supplements</h6>
+          </div>
+          <div class="col-md-2">
+            <img src="../assets/SeniorDog.jpg" alt="" class="pet-img" />
+            <h6>Pets Dental Care</h6>
+          </div>
+          <div class="col-md-2">
+            <img src="../assets/HipJoint-1.jpg" alt="" class="pet-img" />
+            <h6>Senior Pet Care & Pain Relief</h6>
+          </div>
+          <div class="col-md-2">
+            <img src="../assets/FoodTopper.jpg" alt="" class="pet-img" />
+            <h6>All Natural Pet Food Toppers</h6>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="bgPrimary py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="d-flex align-items-center h-100">
+              <div>
+                <h1 class="text-white">Greetings </h1>
+                <p class="text-white">
+                 We offers high quality, comprehensive vet care in locations near you. Anticipate Your Pets' Health Needs and Avoid Costly Treatments. Affordable Rates. Online Booking. Same Day Appointments. High Tech Clinics. Top Customer Service. Low Cost Pricing.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6">
+            <img
+              src="../assets/dog_meow-removebg-preview.png"
+              alt=""
+              class="img-fluid"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Section 5 -->
+    <div class="py-5">
+      <h2 style="text-align: center">Learn more about Animal Care Center</h2>
+      <div class="container py-5">
+        <div class="row">
+          <div class="col-md-3">
+            <learn-more />
+          </div>
+          <div class="col-md-3">
+            <learn-more />
+          </div>
+          <div class="col-md-3">
+            <learn-more />
+          </div>
+          <div class="col-md-3">
+            <learn-more />
+          </div>
+        </div>
+      </div>
+    </div>
   </template-view>
 </template>
 
 <script>
 // @ is an alias to /src
-import CarouselComponent from '@/components/CarouselComponent.vue';
-import TemplateView from '@/views/TemplateView'
-import LearnMore from '@/components/LearnMore.vue'
+import CarouselComponent from "@/components/CarouselComponent.vue";
+import TemplateView from "@/views/TemplateView";
+import LearnMore from "@/components/LearnMore.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
     TemplateView,
     CarouselComponent,
-    LearnMore
-  }
-}
+    LearnMore,
+  },
+};
 </script>
